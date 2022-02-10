65b94f8ba35d4d99ba1d6aac2afe4df1e320a1e4
nopiarahman <nopiarahman93@gmail.com>
2022-02-11 00:09:24 +0700

    install spatie laravel-permission, add basic role 2

---
+++ database/seeders/RoleAndPermission.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleAndPermission extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        // create permissions
+        Permission::updateOrCreate(['name' => 'lihat saldo']);
+        Permission::updateOrCreate(['name' => 'lihat transaksiKeluar']);
+        Permission::updateOrCreate(['name' => 'lihat transaksiMasuk']);
+        Permission::updateOrCreate(['name' => 'lihat kas']);
+        Permission::updateOrCreate(['name' => 'lihat logistik']);
+        // updateOrCreate roles and assign existing permissions
+        $superAdmin = Role::updateOrCreate(['name' => 'Super-Admin']);
+        $keuangan = Role::updateOrCreate(['name' => 'keuangan']);
+        $logistik = Role::updateOrCreate(['name' => 'logistik']);
+
+        /* Assign Permission */
+        $superAdmin->syncPermissions(Permission::all());
+        $keuangan->syncPermissions([
+            'lihat transaksiKeluar',
+            'lihat transaksiMasuk',
+            'lihat kas',
+            'lihat saldo',
+        ]);
+        $logistik->syncPermissions([
+            'lihat logistik'
+        ]);
+    }
+}
